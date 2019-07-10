264785612f8d7b9b85358b11ba4cce68a03324c6
Mioratahina <alymiratana@gmail.com>
2019-07-10 10:58:09 +0300

    these are some rest

---
+++ exo_10.rb
@@ -0,0 +1,4 @@
+print "Ecrire votre année de naissance : "
+annee = gets.to_i
+
+puts "ton âge en 2017 est : #{2017 - annee}"
---
+++ exo_9.rb
@@ -0,0 +1,5 @@
+print "Ecrit ton prénom : " 
+firt_name = gets.chomp
+print "Ecrit ton nom : "
+last_name = gets.chomp
+puts "Bonjour, " + firt_name + " " + last_name + " !"
