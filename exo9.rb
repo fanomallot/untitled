9032a25c1203e4b9c5d33e9f6a966cbf49f6b33c
fano <aafano19@gmail.com>
2019-07-10 10:52:32 +0300

    exo 9 envoyé

---
+++ exo9.rb
@@ -0,0 +1,9 @@
+
+puts "hi,can you write your name please §§ !"
+print ">"
+user_name_1 = gets.chomp
+puts "right,can now you write your Firstname please §§ !"
+print ">"
+user_name_2 = gets.chomp
+
+puts "Bonjour," + user_name_1 + "  "+ user_name_2 + "!"
