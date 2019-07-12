171311557655185f306d9fe0485731434edcf9db
fano <aafano19@gmail.com>
2019-07-12 16:58:02 +0300

    journaliste

---
+++ test.rb
@@ -0,0 +1,95 @@
+#faire sortir les nom
+puts "voisi des nom"
+k = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+puts k
+#donner le nombre de contenu
+t = k.length
+r = 0
+for i in 1..t do
+	r += 1	
+end
+puts "on a #{r} noms au total"
+#donner ceux qui comporte des chiffres
+num = 0
+nombre = 0
+for i in 0..t-1 do
+	l=k[i]
+	f = l.length
+	for j in 0..f-1 do
+		if (k[i][j] == "0" or k[i][j] == "1" or k[i][j] == "2" or k[i][j] == "3" or k[i][j] == "4" or k[i][j] == "5" or k[i][j] == "6" or k[i][j] == "7" or k[i][j] == "8" or k[i][j] == "9"  )
+			num = 1
+		end
+	end
+	if (num == 1 )
+		nombre += 1
+	end
+	num = 0
+end
+puts "on a #{nombre} nom avec des chiffres"
+#donner un nom avec certain caractère donné
+aude = 0
+tot = 0
+for i in 0..t-1 do
+	l=k[i]
+	f = l.length 
+	for j in 0..f-4 do
+	
+		if (k[i][j] == "a" or k[i][j] == "A")
+			if (k[i][j+1] == "u" or k[i][j+1] == "U") 
+				if (k[i][j+2] == "d" or k[i][j+2] == "D")
+					if (k[i][j+3] == "e" or k[i][j+3] == "E")
+						tot += 1
+					end
+				end
+			end
+		end
+
+	end	
+end	
+puts "on a #{tot} noms avec aude"
+#le nom qui commence par des majuscules
+m = 0
+for i in 0..t-1
+	if ( k[i][1] == "A" or k[i][1] == "B" or k[i][1] == "C" or k[i][1] == "D" or k[i][1] == "E" or k[i][1] == "F" or k[i][1] == "G" or k[i][1] == "H" or k[i][1] == "I" or k[i][1] == "J" or k[i][1] == "K" or k[i][1] == "L" or k[i][1] == "M" or k[i][1] == "N" or k[i][1] == "O" or k[i][1] == "P" or k[i][1] == "Q" or k[i][1] == "R" or k[i][1] == "S" or k[i][1] == "T" or k[i][1] == "U" or k[i][1] == "V" or k[i][1] == "W" or k[i][1] == "X" or k[i][1] == "Y" or k[i][1] == "Z")
+		m += 1
+	end
+end
+puts "le nombre de nom débuté par un majuscule est #{m}"
+#nombre de ce qui contient des majuscules
+man = 0
+zzz = 0
+for i in 0..t-1
+	l=k[i]
+	f = l.length 
+	for j in 0..f-1
+		if ( k[i][j] == "A" or k[i][j] == "B" or k[i][j] == "C" or k[i][j] == "D" or k[i][j] == "E" or k[i][j] == "F" or k[i][j] == "G" or k[i][j] == "H" or k[i][j] == "I" or k[i][j] == "J" or k[i][j] == "K" or k[i][j] == "L" or k[i][j] == "M" or k[i][j] == "N" or k[i][j] == "O" or k[i][j] == "P" or k[i][j] == "Q" or k[i][j] == "R" or k[i][j] == "S" or k[i][j] == "T" or k[i][j] == "U" or k[i][j] == "V" or k[i][j] == "W" or k[i][j] == "X" or k[i][j] == "Y" or k[i][j] == "Z")
+			man = 1
+		end
+	end
+	if (man == 1)
+		zzz +=man
+	end
+	man = 0
+end
+puts "le nombre de nom qui contient un majuscule est #{zzz}"
+#nombre des noms qui contient des underscores _ 
+n=0
+underscores=0
+for i in 0..t-1
+	l=k[i]
+	f = l.length
+	for j in 0..f-1
+		if (k[i][j] == "_")
+			n = 1
+		end
+	end
+	if (n == 1)
+		underscores += 1
+	end
+	n = 0
+end
+puts "le nombre de nom avec des underscores est #{underscores}"
+#triage des noms
+  # for i in 0..t-1 
+   #		l=k[i]
+	#f = l.length
